49263c2c7353f7cf32346315fc2af87fa891e362
Alex Haslehurst <alex.haslehurst@gmail.com>
2019-02-09 11:40:24 +0000

    Add project files.

---
+++ Calas.Scrape/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calas.Scrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
